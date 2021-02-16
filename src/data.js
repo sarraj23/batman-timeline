b1c16fabc2f0e30fc231300516ebd988889ab77e
sarraj23 <78157490+sarraj23@users.noreply.github.com>
2021-02-16 20:29:36 +0300

    it needs work

---
+++ src/data.js
@@ -0,0 +1,95 @@
+const data = [
+{
+    text: 'I was born on',
+    date: 'April 18 1915',
+    category: {
+        tag: 'Birthdate',
+        color: '#018f69'
+    },
+    link: {
+        url: 'https://i.etsystatic.com/13077198/r/il/1cd136/1861969346/il_794xN.1861969346_5g68.jpg',
+        text: 'My first photo'
+
+    }
+},
+{
+    text: 'My name is Bruce Wayne, my father Thomas, and my mother Martha, accidentally ventured into Gotham Citys notorious "Crime Alley" and were accosted by a mugger.',
+    date: 'April 10th 1929',
+    category: {
+        tag: 'Tragedy & motivation',
+        color: '#626569'
+    },
+    link: {
+        url: 'https://www.previewsworld.com/SiteImage/MainImage/STL053134',
+        text: 'Tragedy day, Batman born'
+    }
+
+},
+{
+    text: 'Dr. Thompkins and Alfred Pennyworth helped me to become Batman, I studied criminology, forensics, and criminal psychology, and learned from manhunters and martial artists, mastering every fighting style. In time, I forged myself into a living weapon to wage war on crime and injustice.',
+    date: 'May 8th 1930',
+    category: {
+        tag: 'Becoming Batman',
+        color: '#018f69'
+    },
+    link: {
+        url: 'https://www.dccomics.com/sites/default/files/imce/2019/09-SEP/BWNW_Cover_rev_5d927c4b776255.16787249.jpg',
+        text: 'Young Me'
+
+    }
+},
+{
+    text: 'Establishing a secret headquarters in the caves beneath my mansion, I became Batman, a Dark Knight to protect Gotham and its citizens from vice and villainy. Alfred Pennyworth remained his confidant, tending to injuries and offering sage advice - whether requested or not!',
+    date: 'November 11th 1938',
+    category: {
+        tag: 'Batcave',
+        color: '#018f69'
+    },
+    link: {
+        url: 'https://cdnb.artstation.com/p/assets/images/images/024/899/969/large/alex-medina-batcave.jpg?1583894582',
+        text: 'Batcave'
+
+    }
+},
+{
+    text: 'Robin happened, and it was a mistake, I should have never saved him',
+    date: 'January 22nd 1946',
+    category: {
+        tag: 'Robin',
+        color: '#018f69'
+    },
+    link: {
+        url: 'https://screenrant.com/wp-content/uploads/Batman-Slaps-Robin-Meme.jpg',
+        text: 'Robin got what he desreved'
+
+    }
+},
+{
+    text: 'My uniform is bulletproof and fire resistant, featuring a weighted cape and a cowl outfitted with night-vision technology and communications arrays; utility belt contains an arsenal of crime-fighting gear, including various types of offensive Batarangs, de-cel jumplines and grapnels, micro-camera, smoke pellets, acetylene torch, gas mask, rebreather, and flexi-cuffs among other miniaturized non-lethal weapons.',
+    date: 'June 3rd 1946',
+    category: {
+        tag: 'fully customized uniform made by Alfred',
+        color: '#018f69'
+    },
+    link: {
+        url: 'https://static.wikia.nocookie.net/newdcmovieuniverse/images/0/0e/5Epp8aH.jpg/revision/latest?cb=20150421030625',
+        text: 'Batman uniform'
+
+    }
+},
+{
+    text: 'Now ',
+    date: 'Jan 31st 2021',
+    category: {
+        tag: 'Now am coded student, and on a neverending fight with Joker cuz we wont kill each other and we are making money outta this',
+        color: '#018f69'
+    },
+    link: {
+        url: 'https://ynsection.com/wp-content/uploads/2020/05/Batman-Arkham-Asylum-Batcave-1.png',
+        text: 'Batman coding'
+
+    }
+},
+];
+
+export default data; 
